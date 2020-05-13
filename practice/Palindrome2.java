56bc3bc572c3149e7a4d04feae483a56c08f68f4
preety88demo <64266506+preety88demo@users.noreply.github.com>
2020-05-13 10:15:24 -0500

    Add files via upload

---
+++ practice/Palindrome2.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class Palindrome2 {
+
+	public static void main(String[] args) {
+	    
+		
+		String text = "malayalam";
+		String strrev="";
+
+
+		for(int i=text.length()-1;i>=0;i--)
+	    {
+	    strrev=strrev+text.charAt(i);
+	    }
+	System.out.println("Reverse of a string is : "+strrev);
+	if(strrev.equalsIgnoreCase(text))
+		System.out.println("Entered string is palindrome");
+               else
+		System.out.println("Entered string is not a palindrome number ");           
+	}
+
+
+	}
+
+
